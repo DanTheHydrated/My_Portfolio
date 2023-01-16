2543edffba3bf89a642cd6245d86bd3f44c35d99
Dan W <daniel.winn@student.awesomeinc.org>
2023-01-16 20:54:37 +0000

    I finally fixxed my boostrap problem

--- daniel/src/App.js
+++ daniel/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Hero from './Hero';
+import About from './About'
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.js';
+
+
 
 function App() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
--- daniel/src/index.js
+++ daniel/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
 
 
 
