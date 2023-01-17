ca89738996c640b6739af37006175743b637cfd2
Dan W <daniel.winn@student.awesomeinc.org>
2023-01-17 18:19:43 +0000

    fixxed some issues I had but the about section needs another run over

--- daniel/src/About.js
+++ daniel/src/About.js
@@ -2,20 +2,28 @@ import Moon from './images/Moon.jpg'
 
 export default function About() {
     return(
-        <div className="container-fluid">
-            <div className='row'>
-            <h1> About me! </h1>
-                <p> I went to school at Eastern Kentucky University for medical Laboratory Sciences, but I realized that feild wasn't for me. I then attended awesome Inc. where I made a project every week for a tottal of three months! I've always loved making things and the Awesome Inc. program helped me realize how I can create things with code!</p>
+        <div className="container-grid">
+            <div className='row justify-content-center'>
+                <div className='col-6'>
+                    <h1> About me! </h1>
+                    <p> I went to school at Eastern Kentucky University for medical Laboratory Sciences, but I realized that feild wasn't for me. I then attended awesome Inc. where I made a project every week for a tottal of three months! I've always loved making things and the Awesome Inc. program helped me realize how I can create things with code!</p>
                 </div>
-                <div className="Projects">
+            </div>
+            <div className="row justify-content-center">
+                <div className='col-6'>
                     <h3> Some Projects! </h3>
                 </div>
-                <div className="Hobbies">
-                    <img src={Moon} alt='Photo of the moon I took' className='MOON' />                    
-                    <h3 className='side-moon'> My Hobbies! 
-                    <p> One of my favorite hobbies is Star Gazing! I love looking through my Telescope and viewing our Universe, I even took the photo to the Right!  </p>
+            </div>
+            <div className="row justify-content-center moon-bar">
+                <div className='col-sm-12 col-6'>
+                    <img src={Moon} alt='Photo of the moon I took' className='img-fluid float-start MOON' width='300' height='600'/>
+                </div>
+                <div className='col-sm-12 col-6'>
+                    <h3 className='side-moon float-end'> My Hobbies!
+                        <p> One of my favorite hobbies is Star Gazing! I love looking through my Telescope and viewing our Universe, I even took the photo to the Right!  </p>
                     </h3>
                 </div>
+            </div>
         </div>
     )
 }
