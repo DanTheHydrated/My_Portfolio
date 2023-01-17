ffb834af68ab90b25835d62bdf6adf31deabbf3b
Dan W <daniel.winn@student.awesomeinc.org>
2023-01-17 19:56:17 +0000

    fixxed the problem with my cards, I cant use the style tag with react and boostrap. I know this but I seem to forget it all the time

--- daniel/src/About.js
+++ daniel/src/About.js
@@ -1,4 +1,7 @@
 import Moon from './images/Moon.jpg'
+import GitHub from './images/Screen Shot 2023-01-17 at 2.31.08 PM.png'
+import Danstagram from './images/Screen Shot 2022-12-14 at 10.29.08 AM.png'
+
 
 export default function About() {
     return(
@@ -10,8 +13,36 @@ export default function About() {
                 </div>
             </div>
             <div className="row justify-content-center">
-                <div className='col-6'>
+                <div className='col-4'>
                     <h3> Some Projects! </h3>
+                    <div className="card">
+                        <img src={GitHub} className="card-img-top" alt="Github profile Page" />
+                        <div className="card-body">
+                            <h5 className="card-title">Evercraft</h5>
+                            <p className="card-text"> built in python and using Test driven development!</p>
+                            <a href="https://github.com/DanTheHydrated/evercraftPyython" className="btn btn-primary">Check out tht GitHub!</a>
+                        </div>
+                    </div>
+                    </div>
+                    <div className='col-4'>
+                    <div className="card">
+                        <img src={Danstagram} className="card-img-top" alt="Danstagram profile" />
+                        <div className="card-body">
+                            <h5 className="card-title">Danstagram </h5>
+                            <p className="card-text"> Danstagram was my final project I built for the Awesome Inc. program! </p>
+                            <a href="https://danstagram-ad70a.web.app/login" className="btn btn-primary">Check out Danstagram!</a>
+                        </div>
+                    </div>
+                    </div>
+                    <div className='col-4'>
+                    <div className="card">
+                        <img src={GitHub} className="card-img-top" alt="Github profile Page" />
+                        <div className="card-body">
+                            <h5 className="card-title">Tic-Tac-Toe</h5>
+                            <p className="card-text"> Tic-tac-Toe I built in Javascript with a fun twist!</p>
+                            <a href="https://github.com/DanTheHydrated/Tic_Tac_Toe" className="btn btn-primary">Check out tht GitHub!</a>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="row justify-content-center moon-bar">
