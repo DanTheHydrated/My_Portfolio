3886299ab1eff84f6de2d2c434a3460221127b60
Dan W <daniel.winn@student.awesomeinc.org>
2023-01-24 22:21:21 +0000

    Idc if the cards are not the same sizes. im done with them

--- daniel/src/About.js
+++ daniel/src/About.js
@@ -1,6 +1,6 @@
 import Moon from './images/Moon.jpg'
 import GitHub from './images/Screen Shot 2023-01-17 at 2.31.08 PM.png'
-import Danstagram from './images/Screen Shot 2022-12-14 at 10.29.08 AM.png'
+import Danstagram from './images/Screen Shot 2022-12-14 at 10.29.08 AM.jpg'
 
 
 export default function About() {
@@ -13,9 +13,13 @@ export default function About() {
                 </div>
             </div>
             <div className="row justify-content-center">
-                <div className='col-4'>
+                <div className='col-6 justify-content-center'>
                     <h3> Some Projects! </h3>
-                    <div className="card h-250">
+                </div>
+                </div>
+                <div className='row justify-content-center height; 200'>
+                <div className='col-4'>
+                    <div className="card h-auto">
                         <img src={GitHub} className="card-img-top" alt="Github profile Page" />
                         <div className="card-body">
                             <h5 className="card-title">Evercraft</h5>
@@ -23,19 +27,19 @@ export default function About() {
                             <a href="https://github.com/DanTheHydrated/evercraftPyython" className="btn btn-primary">Check out tht GitHub!</a>
                         </div>
                     </div>
-                    </div>
-                    <div className='col-4'>
-                    <div className="card h-250">
-                        <img src={Danstagram} className="card-img-top" alt="Danstagram profile" />
+                </div>
+                <div className='col-4'>
+                    <div className="card h-auto">
+                        <img src={Danstagram} className="card-img-top" alt="Danstagram profile" height='300px'/>
                         <div className="card-body">
                             <h5 className="card-title">Danstagram </h5>
                             <p className="card-text"> Danstagram was my final project I built for the Awesome Inc. program! </p>
                             <a href="https://danstagram-ad70a.web.app/login" className="btn btn-primary">Check out Danstagram!</a>
                         </div>
                     </div>
-                    </div>
-                    <div className='col-4'>
-                    <div className="card h-250">
+                </div>
+                <div className='col-4'>
+                    <div className="card h-auto">
                         <img src={GitHub} className="card-img-top" alt="Github profile Page" />
                         <div className="card-body">
                             <h5 className="card-title">Tic-Tac-Toe</h5>
@@ -47,7 +51,7 @@ export default function About() {
             </div>
             <div className="row justify-content-center moon-bar">
                 <div className='col-sm-12 col-6'>
-                    <img src={Moon} alt='Photo of the moon I took' className='img-fluid float-start MOON' width='300' height='600'/>
+                    <img src={Moon} alt='Photo of the moon I took' className='img-fluid float-start MOON' width='300' height='600' />
                 </div>
                 <div className='col-sm-12 col-6'>
                     <h3 className='side-moon float-end'> My Hobbies!
