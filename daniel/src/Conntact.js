da568813e98f6642cf3db8e8d1b1e266f402f340
Dan W <daniel.winn@student.awesomeinc.org>
2023-01-30 21:56:27 +0000

    this still needs work

--- daniel/src/Conntact.js
+++ daniel/src/Conntact.js
@@ -7,13 +7,22 @@ import linkedin from './src/images/Website.svg';
 export default function Contact() {
 return(
     <div className="container justify-content-center">
-        <h1 className="">
-            Contact Me!
-        </h1>
-        <img src={email}/>
-        <img src={github}/>
-        <img src={phone}/>
-        <img src={linkedin}/>
+        <div className="row">
+            <h1 className="Contact-Me">
+                Contact Me!
+            </h1>
+        </div>
+        <div className="row">
+            <div className="col-3">
+                <a href="mailto:r.daniel.winn@gmail.com"><img src={email} alt='send me an email' /></a>
+            </div>
+            <div className="col-3">
+                <a href="https://github.com/DanTheHydrated"><img src={github} alt='check out my GitHub' /></a>
+            </div>
+            <div className="col-3">
+                <a href="https://www.linkedin.com/in/robert-daniel-winn/"><img src={linkedin} alt='check out my Linked In' /></a>
+            </div>
+        </div>
     </div>
 )
 }
